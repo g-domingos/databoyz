9246c7f33d08d7aa2ae5aae903c5a888bb4bcfbd
Gabriel <gdomingosss@gmail.com>
2023-01-29 12:49:19 -0300

    s

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://g-domingos.github.io/databoyz",
-  build: {
-    outDir: "build",
-  },
+  base: "/vite-deploy-demo/",
 });
