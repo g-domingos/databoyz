0191bec6f2b23b6d175c2b42b411b6d3beb51b4e
Gabriel <gdomingosss@gmail.com>
2023-01-29 12:53:44 -0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "react-tooltip/dist/react-tooltip.css";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <GlobalStyle />
       <Router />
     </BrowserRouter>
